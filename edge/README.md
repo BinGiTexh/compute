71e0783b5436b02d0f55c51a5964c78c450a477f
Malik Cameron <malikcameron95@gmail.com>
2025-03-09 20:57:24 -0700

    Adding support for edge devices

---
+++ edge/README.md
@@ -0,0 +1,2 @@
+## Compiling a list of sources of best practices when working with Jetsons
+https://docs.ultralytics.com/guides/nvidia-jetson/#best-practices-when-using-nvidia-jetson
---
+++ edge/yolo.py
@@ -0,0 +1,43 @@
+from ultralytics import YOLO
+import cv2
+
+# Load a model
+model = YOLO("yolov8n-pose.pt")  # load an official model
+
+# Export the model to TensorRT
+model.export(format="engine")  # creates 'yolo11n.engine'
+
+# Load the exported TensorRT model
+trt_model = YOLO("yolo11n.engine")
+
+# Open the video file
+#video_path = "/Users/mcameron/yolo/IMG_9349.MOV"
+#submit video
+# video_path
+cap = cv2.VideoCapture(video_path)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = trt_model(frame)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
